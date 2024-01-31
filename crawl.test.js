5489a98b0a0bacbe0c9e6899c68e5cc3aa9e4768
unknown <kevin.z.zhou@gmail.com>
2024-01-30 23:50:58 -0800

    Implement getURLSFromHTML function

--- crawl.test.js
+++ crawl.test.js
@@ -1,29 +1,54 @@
-import { normalizeURL } from "./crawl";
+import exp from "constants";
+import { normalizeURL, getURLSFromHTML } from "./crawl";
 
-test("normalizeUrl strip protocol", () => {
-  const input = "https://test.dev/path";
-  const output = normalizeURL(input);
-  const expectedOutput = "test.dev/path";
-  expect(output).toEqual(expectedOutput);
+const INPUT_URL = "https://test.dev/path";
+const EXPECTED_URL = "test.dev/path";
+
+describe("normalizeUrl function", () => {
+  test("normalizeUrl strip protocol", () => {
+    const input = "https://test.dev/path";
+    const output = normalizeURL(input);
+    const expectedOutput = "test.dev/path";
+    expect(output).toEqual(expectedOutput);
+  });
+
+  test("normalizeUrl strip trailing slash in path", () => {
+    const input = "https://test.dev/path/";
+    const output = normalizeURL(input);
+    const expectedOutput = "test.dev/path";
+    expect(output).toEqual(expectedOutput);
+  });
+
+  test("normalizeUrl capitals ", () => {
+    const input = "https://Test.Dev/path";
+    const output = normalizeURL(input);
+    const expectedOutput = "test.dev/path";
+    expect(output).toEqual(expectedOutput);
+  });
+
+  test("normalizeUrl strip http ", () => {
+    const input = "http://Test.Dev/path";
+    const output = normalizeURL(input);
+    const expectedOutput = "test.dev/path";
+    expect(output).toEqual(expectedOutput);
+  });
 });
 
-test("normalizeUrl strip trailing slash in path", () => {
-  const input = "https://test.dev/path/";
-  const output = normalizeURL(input);
-  const expectedOutput = "test.dev/path";
-  expect(output).toEqual(expectedOutput);
-});
+describe("getURLSFromHTML function", () => {
+  test("get urls from html", () => {
+    const inputHTMLBody = `
+    <html>
+      <body>
+        <a href="${INPUT_URL}">
+          test.dev
+        </a>
+      </body>
+    </html>
+    `;
 
-test("normalizeUrl capitals ", () => {
-  const input = "https://Test.Dev/path";
-  const output = normalizeURL(input);
-  const expectedOutput = "test.dev/path";
-  expect(output).toEqual(expectedOutput);
-});
-
-test("normalizeUrl strip http ", () => {
-  const input = "http://Test.Dev/path";
-  const output = normalizeURL(input);
-  const expectedOutput = "test.dev/path";
-  expect(output).toEqual(expectedOutput);
+    const inputBaseUrl = INPUT_URL;
+    const actual = getURLSFromHTML(inputHTMLBody, inputBaseUrl);
+    const expected = [INPUT_URL];
+    expect(actual).toEqual(expected);
+  });
 });
